32ec5b325cbb02bef59404e288440b4c57ca8220
Your Name <sobolevi1781@gmail.com>
2020-02-10 19:07:04 +0300

    some new unimportant changes

--- FindInGit/Stargazer.py
+++ FindInGit/Stargazer.py
@@ -23,15 +23,14 @@ class Stargazer:
     def get_user(self, name, passw):
         list_name_passw = [name, passw]
         self.__login_passwd = tuple(list_name_passw)
-        return self.__login_passwd
 
     # The method returns a DataFrame that contains information from a file with a list of repositories
     def get_file_csv(self):
         try:
-            df = pd.read_csv(os.path.abspath(self.__path_to_file), header=None, error_bad_lines=False)
+            df = pd.read_csv(os.path.abspath(self.__path_to_file), error_bad_lines=False, header=None)
             self.__length_of_column = df.shape[0]
             return df
-        except FileNotFoundError:
+        except IOError:
             return -1
 
     # The method receives a DataFrame, which is returned by the get_file_csv () method.
@@ -39,11 +38,13 @@ class Stargazer:
     # If the repository is empty, then the line is filled with the values 'Not', and
     # the value '-1' is set in the column 'stars'. All data is saved to the list __list_of_star = [].
 
-    def __star_count(self, df):
+    def star_count(self, df):
         count = 0
         while count < self.__length_of_column:
+            if df[0][count] == '0' or df[0][count] == 'nan':
+                count += 1
+                continue
             url = df[0][count]
-            print(count)
             try:
                 r = requests.get(url, auth=self.__login_passwd)
                 if r.status_code == 404:
@@ -56,28 +57,24 @@ class Stargazer:
                     self.__list_of_star.append(star)
             except requests.RequestException:
                 self.__list_of_star.append(-1)
-                df.iloc[count] = {'0': 'Not', '1': 'Not', '2': 'Not'}
+                not_complete = {}
+                for a in range(df.shape[1]):
+                    not_complete[a] = 'Not'
+                df.iloc[count] = not_complete
             finally:
                 count += 1
         d = self.__length_of_column - len(self.__list_of_star)
         while d > 0:
             self.__list_of_star.append(-2)
             d -= 1
-        return self.__list_of_star
 
     # Record filled __list_of_star = [] containing the number
     # repository stars to the original csv file with repositories.
     def add_column_to_csv(self, df):
-        self.__star_count(df)
-        df['stars'] = self.__list_of_star
+        df[df.shape[1]] = self.__list_of_star
         try:
-            df.to_csv(self.__path_to_file, index=False)
-        except OSError:
-            return 'Не удалось записать файл'
-        return True
+            df.to_csv(self.__path_to_file, index=False, header=None)
+            return True
+        except IOError:
+            return -1
 
-
-es = Stargazer('G:/finder_in_github/gggG.csv')
-es.get_user('m9gadeth1994', 'ironmaiden20152015')
-f = es.get_file_csv()
-# a = es.add_column_to_csv(f)
