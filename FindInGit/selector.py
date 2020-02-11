426fdace876552ab605e34a0c8dc924b1ff09874
Your Name <sobolevi1781@gmail.com>
2020-02-11 23:02:20 +0300

    add functions garabage_deleter ...

--- FindInGit/selector.py
+++ FindInGit/selector.py
@@ -1,10 +1,12 @@
 import os
 import pandas as pd
 import requests
+import csv
+from git import Repo
 
 
 # The task of the class is to count  the stars of the repositories, the list of which lies in
-# the .csv file and add to this file a new column 'stars' containing the number of stars of
+# the .csv file and add to this file a new column containing the number of stars of
 # the repository
 # We send the path to the constructor to the file where the links to the gitHub repositories
 # are stored.
@@ -14,10 +16,13 @@ class Stargazer:
     __path_to_file = 0
     __login_passwd = 0
 
+    def list_of_star(self):
+        return self.__list_of_star
+
     def __init__(self, path):
         self.__path_to_file = path
 
-    # The get_user () method receives user login and password, then returns tuple
+    # The get_user() method receives user login and password, then returns tuple
     # and saves it in the __login_passwd field. This is then required to send an authorized
     # request to gitHub.
     def get_user(self, name, passw):
@@ -27,7 +32,8 @@ class Stargazer:
     # The method returns a DataFrame that contains information from a file with a list of repositories
     def get_file_csv(self):
         try:
-            df = pd.read_csv(os.path.abspath(self.__path_to_file), error_bad_lines=False, header=None)
+            df = pd.read_csv(os.path.abspath(self.__path_to_file), error_bad_lines=False, header=None,
+                             index_col=False)
             self.__length_of_column = df.shape[0]
             return df
         except IOError:
@@ -37,7 +43,6 @@ class Stargazer:
     # Next, the DataFrame is crawled. Requests are sent to the links contained in the dataFrame.
     # If the repository is empty, then the line is filled with the values 'Not', and
     # the value '-1' is set in the column 'stars'. All data is saved to the list __list_of_star = [].
-
     def star_count(self, df):
         count = 0
         while count < self.__length_of_column:
@@ -78,3 +83,57 @@ class Stargazer:
         except IOError:
             return -1
 
+
+def transform_url(url):
+    url = url.split('/')
+    url.pop(2)
+    url.insert(2, 'github.com')
+    url.pop(3)
+    url = '/'.join(url)
+    return url
+
+
+def download_repo(path_file_csv, to_path):
+    with open(os.path.abspath(path_file_csv), 'r', encoding='utf_8_sig') as file_repo:
+        reader = csv.reader(file_repo)
+        for row in reader:
+            if 3 > int(row[len(row) - 1]) >= 0:
+                url = transform_url(row[0])
+                print(url)
+                name_repo = url.split('/')
+                repo = name_repo.pop(len(name_repo) - 1)
+                path1 = to_path + '/' + repo
+                print(path1)
+                try:
+                    os.mkdir(path1)
+                    repo = Repo.clone_from(url, path1)
+                except Exception:
+                    pass
+            else:
+                pass
+
+
+def get_list_of_files(dir_name):
+    list_dir = os.listdir(os.path.join(dir_name))
+    all_files = list()
+    for entry in list_dir:
+        full_path = os.path.join(dir_name, entry)
+        if os.path.isdir(full_path):
+            all_files += get_list_of_files(full_path)
+        else:
+            all_files.append(full_path)
+    return all_files
+
+
+def garbage_deleter(dir_name):
+    list_files = get_list_of_files(dir_name)
+    languages = ('.py', '.java', '.cpp', '.js', '.php', '.cs', '.rb', '.go')
+    list_files = filter(lambda s: not s.endswith(languages), list_files)
+    for file in list_files:
+        if os.path.exists(file):
+            try:
+                os.remove(file)
+            except PermissionError:
+                os.chmod(file, 0o777)
+                os.remove(file)
+    return list_files
