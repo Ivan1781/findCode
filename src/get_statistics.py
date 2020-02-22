4c61bd2edcf7b6343a4242bc98007f4885a21f76
Your Name <sobolevi1781@gmail.com>
2020-02-22 23:06:26 +0300

    the module for counting some statistics

--- src/get_statistics.py
+++ src/get_statistics.py
@@ -1,49 +1,44 @@
-from src.download_clean import get_list_of_files
+import os
 import re
+import download_clean
+
 
 languages = {'Python': ['.py', '.pyc', '.pyo', '.pyd', '.whl'],
              'Ruby': ['.rb', '.rbw'], 'Go': ['.go'],
-             'Java': ['.java', '.j', '.jav'], 'JavaScript':['js'],
+             'Java': ['.java', '.j', '.jav'], 'JavaScript': ['.js'],
              'C': ['.c', '.h'], 'C++': ['.cpp', '.cc', '.cxx', '.c++', '.hh', '.hpp'],
-             'C#': ['cs'], 'PHP': ['.php', '.phps']
+             'C#': ['.cs'], 'PHP': ['.php', '.phps']
              }
 
 
 def to_count_rows_in_file(path_to_file):
-    with open(path_to_file, 'r') as file:
-        number_rows = 0
-        while True:
-            row = file.readline()
-            row = row.lstrip()
-            if len(row) == 0:
-                break
-            elif len(row) == 1:
-                pass
-            else:
-                number_rows += 1
-        return number_rows
+    with open(path_to_file, 'r', encoding='utf-8',  errors='ignore') as file:
+        rows = file.readlines()
+        numbers_of_rows = len(rows)
+    return numbers_of_rows
 
 
-def to_count_rows_in_repo(path_to_repo):
-    all_files_repo = get_list_of_files(path_to_repo)
+def to_count_rows_in_repo(list_files):
     count_rows = 0
-    for file in all_files_repo:
+    for file in list_files:
         count_rows += to_count_rows_in_file(file)
     return count_rows
 
 
+# The function counts the number of repository files
 def numbers_of_files_repo(path_to_repo):
-    return len(get_list_of_files(path_to_repo)) - 1
+    return len(download_clean.get_list_of_files(path_to_repo))
 
 
+# The function counts the number of project languages
 def technologies_of_project(list_of_repo_file):
     langs_of_repo = set()
-    lang_pattern = r'\.[a-z]*'
     for file in list_of_repo_file:
-        extended_file = re.search(lang_pattern, file)
+        parts_file = file.split('.')
+        extension = '.' + parts_file[len(parts_file)-1]
         for lang in languages:
-            if extended_file.group() in languages.get(lang):
+            if extension in languages.get(lang):
                 langs_of_repo.add(lang)
             else:
                 pass
-    return langs_of_repo
+    return len(langs_of_repo)
